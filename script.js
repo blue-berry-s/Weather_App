8d6e6fdbb97e7662d0d06ac286c581e9486ff39a
blue-berry-s <95601084+blue-berry-s@users.noreply.github.com>
2024-06-17 16:12:56 -0400

    Forecast functionality added

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ const percip_text = document.querySelector('.percipi_probability');
 const hi_text = document.getElementById('high_text');
 const lo_text = document.getElementById('low_text');
 
+const bg1 = document.getElementById('alt_1');
+const bg2 = document.getElementById('alt_2');
+
+let changebg = 1;
+
+
+const main_img = document.getElementById('weather_icon');
+const weather_cond_text = document.getElementById('weather_cond_text');
+
+const future_predictions = document.getElementsByClassName('future_forecast_tabs');
+
+
 const LOCAPIBEFORE = 'https://geocode.maps.co/search?q=';
 const LOCAPIKEY = '&api_key=6669af5c5b2d4496675341brc2a1bbc';
 
 let longitude = 0;
 let latitude = 0;
 let CURRENTWEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability,weather_code&daily=temperature_2m_max,temperature_2m_min&forecast_days=1`;
+let FUTUREWEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,apparent_temperature_max,precipitation_probability_max`;
+
 
 search_bar.addEventListener("submit", findLocation)
 
@@ -30,6 +44,7 @@ function findLocation(event){
             longitude = location[0].lon;
             latitude = location[0].lat;
             findWeather();
+            findFutureWeather();
         }
         catch(error){ 
             window.alert("No location found :(");
@@ -55,25 +70,8 @@ function getCountry(text){
 }
 
 
-function getLocationText(entered, country){
-
-    const fixed = capitalize(entered).trim();
-    const split_entered = fixed.includes(',') ? text.split(/,\s|,/) : [fixed]
-    
-
-    if (split_entered[split_entered.length - 1].toLowerCase() == country.trim().toLowerCase()){
-        return fixed;
-    }
-    else{
-        return fixed + ", " + country.trim();
-    }
-
-}
-
-
-
 function findWeather(){
-    console.log(CURRENTWEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability,weather_code&daily=temperature_2m_max,temperature_2m_min&forecast_days=1`);
+    CURRENTWEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability,weather_code&daily=temperature_2m_max,temperature_2m_min&forecast_days=1`;
     let fetchWeather = fetch(CURRENTWEATHER);
     fetchWeather.then(response => {
         return response.json();
@@ -82,14 +80,8 @@ function findWeather(){
 
     }).then(weather =>{
         try{
-            temp_text.innerText = Math.round(weather.hourly.temperature_2m[0]); 
-            temp_text.append(createDegrees());
-            percip_text.innerHTML = "P: " + weather.hourly.precipitation_probability[0] + "&#37;";
-            hi_text.innerHTML = "H: " + Math.round(weather.daily.temperature_2m_max[0]);
-            hi_text.append(createDegrees());
-            lo_text.innerHTML = "L: " + Math.round(weather.daily.temperature_2m_min[0]);
-            lo_text.append(createDegrees());
-
+            updateMainWeather(weather)
+        
         }
         catch(error){ 
             window.alert(error);
@@ -100,6 +92,23 @@ function findWeather(){
     })
 }
 
+function findFutureWeather(){
+    FUTUREWEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,apparent_temperature_max,precipitation_probability_max`;
+    let fetchFuture = fetch(FUTUREWEATHER);
+    fetchFuture.then(response => {
+        return response.json();
+    }, error =>{ 
+        window.alert(error);
+
+    }).then(weather =>{
+        updateFutureWeather(weather)
+    }, error =>{ 
+        window.alert(error);
+
+    })
+
+}
+
 function createDegrees(){
     const degreeSign = document.createElement("span");
     degreeSign.innerHTML = '&deg;';
@@ -107,6 +116,71 @@ function createDegrees(){
     return degreeSign;
 }
 
+//the function of updating temperature/percipitation could become it's own function
+
+
+function updateMainWeather(weather){
+    temp_text.innerText = Math.round(weather.hourly.temperature_2m[0]); 
+            temp_text.append(createDegrees());
+            percip_text.innerHTML = "P: " + weather.hourly.precipitation_probability[0] + "&#37;";
+            hi_text.innerHTML = "H: " + Math.round(weather.daily.temperature_2m_max[0]);
+            hi_text.append(createDegrees());
+            lo_text.innerHTML = "L: " + Math.round(weather.daily.temperature_2m_min[0]);
+            lo_text.append(createDegrees());
+
+            const weather_code = weather.hourly.weather_code[0];
+            
+            const weather_obj = weather_code_dict[weather_code];
+
+            main_img.src = weather_obj.img_src;
+            weather_cond_text.innerText = weather_obj.weather_cond_text;
+
+            if (changebg == 0){
+                bg1.style.background = weather_obj.color_gradient_main;
+                bg1.classList.add('show');
+                bg1.classList.remove('hide');
+                bg2.classList.add('hide');
+                bg2.classList.remove('show');
+                changebg = 1;
+            }
+            else {
+                bg2.style.background = weather_obj.color_gradient_main;
+                bg2.classList.add('show');
+                bg2.classList.remove('hide');
+                bg1.classList.add('hide');
+                bg1.classList.remove('show');    
+                changebg = 0;
+            }
+            
+}
+
+
+function updateFutureWeather(weather){
+
+    console.log(future_predictions.length);
+    const today = new Date();
+    let days_ahead = 1;
+    for (var i = 0; i < future_predictions.length; i ++){
+        let date_text = future_predictions[i].querySelector('.days');
+        let temperature_text = future_predictions[i].querySelector('.tempature');
+        let percip_text = future_predictions[i].querySelector('.percipi_probability');
+        let weather_icon = future_predictions[i].querySelector('.forecast_img');
+
+
+        date_text.innerHTML = i+1;
+        temperature_text.innerHTML = Math.round(weather.daily.apparent_temperature_max[i+1]); 
+        temperature_text.append(createDegrees());
+
+        percip_text.innerHTML = "P: " + weather.daily.precipitation_probability_max[i+1] + "&#37;";
+
+        let weather_obj = weather_code_dict[weather.daily.weather_code[i+1]];
+
+        weather_icon.src = weather_obj.img_src;
+
+        future_predictions[i].style.background = weather_obj.color_gradient_predict;
+    }
+
+}
 
 
 
--- weather.js
+++ weather.js
@@ -1,5 +1,70 @@
 class Weather{
-    constructor(img_src, color_gradient,){
-
+    constructor(weather_cond_text, img_src, color_gradient_main, color_gradient_predict){
+        this.weather_cond_text = weather_cond_text;
+        this.img_src = img_src;
+        this.color_gradient_main = color_gradient_main;
+        this.color_gradient_predict = color_gradient_predict;
     }
+        
 }
+
+const weather_code_0 = new Weather('Clear sky', 'Images\\Sunny_Icon.png', 'linear-gradient(180deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)', 'linear-gradient(90deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)');
+const weather_code_1 = new Weather('Mainly clear', 'Images\\Sun_Small_Cloud_Cover_Icon.png', 'linear-gradient(180deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)', 'linear-gradient(90deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)');
+const weather_code_2 = new Weather('Partly cloudy', 'Images\\Sun_Large_Cloud_Cover_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_3 = new Weather('Cloudy', 'Images\\Cloudy_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_45 = new Weather('Fog', 'Images\\Cloudy_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_48 = new Weather('Depositing rime fog', 'Images\\Cloudy_Icon.png', 'linear-gradient(180deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)', 'linear-gradient(90deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)');
+const weather_code_51 = new Weather('Light drizzle', 'Images\\Sun_Rain_Icon.png', 'linear-gradient(180deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)', 'linear-gradient(90deg, rgba(139,227,255,1) 0%, rgba(28,72,226,1) 100%)');
+const weather_code_53 = new Weather('Moderate drizzle', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_55 = new Weather('Dense drizzle', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_56 = new Weather('Light Freezing drizzle', 'Images\\Sun_Rain_Icon.png', 'linear-gradient(180deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)', 'linear-gradient(90deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)');
+const weather_code_57 = new Weather('Dense Freezing drizzle', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_61 = new Weather('Slight Rain', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_63 = new Weather('Moderate Rain', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_65 = new Weather('Heavy Rain', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)', 'linear-gradient(90deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)');
+const weather_code_66 = new Weather('Slight Freezing Rain', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_67 = new Weather('Heaving Freezing Rain', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_71 = new Weather('Slight Snow', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)', 'linear-gradient(90deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)');
+const weather_code_73 = new Weather('Moderate Snow', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_75 = new Weather('Heavy Snow', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_77 = new Weather('Snow grains', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)', 'linear-gradient(90deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)');
+const weather_code_80 = new Weather('Slight Rain Shower', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_81 = new Weather('Moderate Rain Shower', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)', 'linear-gradient(90deg, rgba(40,104,150,1) 0%, rgba(115,170,137,1) 100%)');
+const weather_code_82 = new Weather('Violent Rain Shower', 'Images\\Rain_Icon.png', 'linear-gradient(180deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)', 'linear-gradient(90deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)');
+const weather_code_85 = new Weather('Slight Snow Showers', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)', 'linear-gradient(90deg, rgba(72,56,255,1) 0%, rgba(208,249,255,1) 100%)');
+const weather_code_86 = new Weather('Heaby Snow Showers', 'Images\\Snowing_Icon.png', 'linear-gradient(180deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)', 'linear-gradient(90deg, rgba(30,113,173,1) 0%, rgba(235,226,201,1) 100%)');
+const weather_code_95 = new Weather('Thunderstorm', 'Images\\Thunder_Storm_Icon.png', 'linear-gradient(180deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)', 'linear-gradient(90deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)');
+const weather_code_96 = new Weather('Thunderstorm with slight hail', 'Images\\Thunder_Storm_Icon.png', 'linear-gradient(180deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)', 'linear-gradient(90deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)');
+const weather_code_99 = new Weather('Thunderstorm with heavy hail', 'Images\\Thunder_Storm_Icon.png', 'linear-gradient(180deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)', 'linear-gradient(90deg, rgba(0,9,55,1) 0%, rgba(146,119,224,1) 100%)');
+
+const weather_code_dict = {
+    0:  weather_code_0,
+    1:  weather_code_1,
+    2:  weather_code_2,
+    3:  weather_code_3,
+    45: weather_code_45,
+    48:	weather_code_48,
+    51: weather_code_51,
+    53: weather_code_53,
+    55:	weather_code_55,
+    56: weather_code_56,
+    57:	weather_code_57,
+    61: weather_code_61,
+    63: weather_code_63,
+    65:	weather_code_65,
+    66: weather_code_66,
+    67: weather_code_67,
+    71: weather_code_71,
+    73: weather_code_73,
+    75: weather_code_75,
+    77: weather_code_77,
+    80: weather_code_80,
+    81: weather_code_81,
+    82:	weather_code_82,
+    85: weather_code_85,
+    86:	weather_code_86,
+    95: weather_code_95,
+    96: weather_code_96,
+    99: weather_code_99
+}
+
